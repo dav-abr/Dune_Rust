aaf0bc6f411c31accaf0429274b688797ff5b209
Abrahamyan <davo.abrahamyan12@gmail.com>
2024-01-27 22:46:59 +0400

    Added change cursor type if creature is selected

--- src/hud.rs
+++ src/hud.rs
@@ -19,7 +19,7 @@ impl Plugin for HUDPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<SelectedCursorTimer>()
             .add_systems(Startup, setup_cursor)
-            .add_systems(Update, move_cursor)
+            .add_systems(Update, (move_cursor, change_cursor))
             .add_systems(Startup, setup_select_cursor)
             .add_systems(Update, (update_select_cursor, tick_select_cursor_timer));
     }
@@ -37,6 +37,13 @@ fn setup_cursor(
         ImageBundle {
             image: asset_server.load("cursor.png").into(),
             style: Style {
+                width: Val::Px(CELL_SIZE),
+                height: Val::Px(CELL_SIZE),
+                margin: UiRect {
+                    left: -Val::Px(CELL_SIZE / 2.0),
+                    top: -Val::Px(CELL_SIZE / 2.0),
+                  ..default()
+                },
                 position_type: PositionType::Absolute,
                 ..default()
             },
@@ -54,8 +61,24 @@ fn move_cursor(window: Query<&Window>, mut cursor: Query<&mut Style, With<GameCu
         let mut img_style = cursor.single_mut();
 
         // todo: Change to cursor image width and height
-        img_style.left = Val::Px(position.x - 16.0);
-        img_style.top = Val::Px(position.y - 16.0);
+        img_style.left = Val::Px(position.x);
+        img_style.top = Val::Px(position.y);
+    }
+}
+
+fn change_cursor(
+    selected_creature_query: Query<Entity, With<SelectedCreature>>,
+    mut cursor_query: Query<&mut UiImage, With<GameCursor>>,
+    asset_server: Res<AssetServer>,
+) {
+    if let Ok(mut cursor_image) = cursor_query.get_single_mut() {
+        let is_creature_selected = selected_creature_query.get_single().is_ok();
+
+        if is_creature_selected {
+            *cursor_image = asset_server.load("cursor_selected.png").into();
+        } else {
+            *cursor_image = asset_server.load("cursor.png").into();
+        }
     }
 }
 
@@ -80,12 +103,12 @@ fn setup_select_cursor(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn update_select_cursor(
-    select_cursor_query: Query<&Transform, (With<SelectedCreature>, Without<SelectCursor>)>,
-    mut selected_creature_query: Query<(&mut Visibility, &mut Transform), With<SelectCursor>>,
+    selected_creature_query: Query<&Transform, (With<SelectedCreature>, Without<SelectCursor>)>,
+    mut select_cursor_query: Query<(&mut Visibility, &mut Transform), With<SelectCursor>>,
     select_cursor_blink_timer: Res<SelectedCursorTimer>,
 ) {
-    if let Ok((mut select_cursor_visibility, mut selected_cursor_transform)) = selected_creature_query.get_single_mut() {
-        if let Ok(selected_creature_transform) = select_cursor_query.get_single() {
+    if let Ok((mut select_cursor_visibility, mut selected_cursor_transform)) = select_cursor_query.get_single_mut() {
+        if let Ok(selected_creature_transform) = selected_creature_query.get_single() {
             let translation = selected_creature_transform.translation.clone();
 
             selected_cursor_transform.translation.x = translation.x;
